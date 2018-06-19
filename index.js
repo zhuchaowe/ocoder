6855f7960687ccc4ee24bd21d23d3d6a6bab81eb
zhuchao <zhuchao@iosx.me>
2018-06-19 19:01:53 +0800

    icon

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 #!/usr/bin/env node
+var fs = require('fs');
 var program = require('commander');
 var css = require('./css/css.js');
-
-program
-  .command('css <cssFile> <outPath>')
-  .action(css);
-
+var icon = require('./icon/icon.js');
+program.version(JSON.parse(fs.readFileSync(__dirname + '/package.json', 'utf8')).version);
+program.command('css <cssFile> <outPath>').action(css);
+program.command('icon <iconFile> <outPath>').action(icon);
 program.parse(process.argv);
 
 
